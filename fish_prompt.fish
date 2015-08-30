338b06b26b4fc389552728aea53996137d6a2d69
Ken Gullaksen <ken.gullaksen@embriq.no>
2015-08-31 00:47:07 +0200

    prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     set_color $fish_color_cwd
-    echo -n (basename (prompt_pwd))
+    echo -n (prompt_pwd)
     git-radar --fish -fetch
     set_color normal
     echo -n ' > '
